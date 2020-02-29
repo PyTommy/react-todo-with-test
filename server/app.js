383e481fba674971e921a5e2b7addbf658dec134
Your Name <you@example.com>
2020-02-29 13:40:10 +0900

    Set up npm scripts for dev and virtual production

--- server/app.js
+++ server/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('', (req, res) => {
+app.get('/api/', (req, res) => {
     res.send('From server!!');
 });
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
